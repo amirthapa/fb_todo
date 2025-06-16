a9c0ff693378a51ddf5dcb8636d21a5d5ffa66a5
Amir Thapa <162610543+v-Athapa@users.noreply.github.com>
2025-06-16 15:59:51 +0545

    feat: Initialize Firebase, Replace google_service.json and GoogleService-info.plist file after enable google authonication provider in firebase console

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:stacked_services/stacked_services.dart';
 import 'package:todo/app/app.bottomsheets.dart';
 import 'package:todo/app/app.dialogs.dart';
 import 'package:todo/app/app.locator.dart';
 import 'package:todo/app/app.router.dart';
-import 'package:stacked_services/stacked_services.dart';
+import 'package:todo/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setupLocator();
+
   setupDialogUi();
   setupBottomSheetUi();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
